a2eeb25bd3df269e3fb7d184aee658fe90d5e17d
josias ASSIH <josiasassih4@gmail.com>
2024-09-05 14:46:42 +0000

    Debut To Do List

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
+
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref }  from 'vue'
+  let id = 0
+  const ListeTache = ref([
+    {id: id++,nom:'Apprendre PHP'},
+    {id: id++,nom:'Apprendre Vue'},
+    {id: id++,nom:'Apprendre JAVA'}
+  ])
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>To Do List</h1>
+  <input type="text"/>
+  <button>Ajouter</button>
+  <br/>
+  <h3>Liste des Tâches</h3>
+  <ul>
+    <li v-for="tache in ListeTache " :key="tache.id">
+      {{tache.id}} {{tache.nom}}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
